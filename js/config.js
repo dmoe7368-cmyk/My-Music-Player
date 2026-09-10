25ff197428619e1e4774513bfedbb68968e1cb6d
dmoe7368-cmyk <dmoe7368@gmail.com>
2026-09-10 13:46:27 +0630

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,14 +2,13 @@
 // PUBLIC GOOGLE DRIVE CONFIG
 // ============================================
 
-const DRIVE_CONFIG = {
+window.DRIVE_CONFIG = {
 
   // Your public Google Drive folder ID
   folderId: "1903JCYkLeqt6MJ7bFWO9G4Ikgr0bbKY2",
 
   // Google Drive API Key
-  // Replace this with your Google API Key
-  apiKey: "AIzaSyCmpoImAy7O8brST6lBFWBupa_AwEeeEGo",
+  apiKey: "YOUR_API_KEY_HERE",
 
   // Automatically load music from the folder
   autoLoad: true
